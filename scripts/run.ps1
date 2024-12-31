e1596f8e97138c1a7f5bf8dedc3a77827dab9a07
Mohamed Hassan <moh.hassan20@gmail.com>
2024-12-31 04:27:33 +0200

    Update appveyor.yml

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -1,18 +1,24 @@
 Function Download-Artifacts {
-    Write-Host "Downloading artifacts..." -ForegroundColor Yellow
     $CI_USER_TOKEN = $env:SIGNPATH_CI_USER_TOKEN
     $SIGNING_REQUEST_ID = $env:SIGNPATH_SIGNING_REQUESt_ID
     $ORGANIZATION_ID = $env:SIGNPATH_ORGANIZATION_ID
-	$output= $env:ARTIFACTS_PATH
+	$output_file= $env:ARTIFACTS_PATH
+    Write-Host "Downloading artifacts to $output_file" -ForegroundColor Yellow
 
     $headers = @{
         "Authorization" = "Bearer $($CI_USER_TOKEN)"
     }
+
     $ProgressPreference = 'SilentlyContinue'   
 
     Invoke-RestMethod -Uri "https://app.signpath.io/API/v1/$ORGANIZATION_ID/SigningRequests/$SIGNING_REQUEST_ID/SignedArtifact" `
         -Headers $headers `
-        -OutFile $Output
-    Write-Host "Dwnloading complete, path: $Output"
+        -OutFile $output_file
+    #test if the file exists
+    if (-not (Test-Path $output_file)) {
+        Write-Host "Failed to download artifacts" -ForegroundColor Red
+        exit 1
+    }    
+    Write-Host "Dwnloading complete, path: $output_file"
 }
 
